e2d996885a146968c0efab2f1637d94fd52666c7
SoohoPark <clapp301@gmail.com>
2024-02-29 21:02:19 +0900

    initial commit

---
+++ src/main/java/clapho/commutingsystem/CommutingSystemApplication.java
@@ -0,0 +1,13 @@
+package clapho.commutingsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommutingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommutingSystemApplication.class, args);
+    }
+
+}
